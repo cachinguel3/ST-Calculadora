a68ddbbfea2e570b1adb10faa82e811142db91c8
Eduardo_A. <edu.crecenzio.1003@gmail.com>
2026-03-29 21:42:09 -0300

    some changes

--- app/src/main/java/com/example/testeconfiguracao/MainActivity.java
+++ app/src/main/java/com/example/testeconfiguracao/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.testeconfiguracao;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnNum1;
+
+    Button btnNum2;
+
+    Button btnNum3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
